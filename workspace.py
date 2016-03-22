5b187c628fbca09033f3188629e8eb30aa251dfc
Alberto Bacchelli <a.bacchelli@tudelft.nl>
2016-03-22 12:15:19 +0100

    Log to a file

--- workspace.py
+++ workspace.py
@@ -2,27 +2,28 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from csmmodel.base import Base
 import csmmodel.csvenue
-#import csmmodel.csvenueedition
-#import csmmodel.cspublication
-#import csmmodel.author_pub_association
-#import csmmodel.csauthor
+# import csmmodel.csvenueedition
+# import csmmodel.cspublication
+# import csmmodel.author_pub_association
+# import csmmodel.csauthor
 from sqlalchemy.orm import sessionmaker
 import xml.sax
 import dblp.wrapper
 import logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+logging.basicConfig(filename='output.log', level=logging.INFO,
+                    format='%(asctime)s %(message)s')
 logging.getLogger().setLevel(logging.DEBUG)
-
-
 
 logging.debug('Starting the parsing process')
 
 database_directory = "data/"
 
-#engine = create_engine('sqlite:///' + database_directory + '/data.db',
+# engine = create_engine('sqlite:///' + database_directory + '/data.db',
 #                       echo=False)
 
-engine = create_engine('postgresql://sback:piripicchio@localhost:5432/cscientists')
+engine = create_engine(
+    'postgresql://sback:piripicchio@localhost:5432/cscientists')
 
 Base.metadata.drop_all(engine)
 
@@ -35,7 +36,6 @@ session = Session()
 
 source = open(database_directory + "/tiny_dblp.xml", 'r')
 xml.sax.parse(source, dblp.wrapper.DBLPContentHandler(session))
-
 
 # venue = csmmodel.csvenue.CsVenue(abbr='infocomm',
 #                                  type='conference',
@@ -67,4 +67,3 @@ session.commit()
 session.close()
 
 logging.debug('Finished the parsing process')
-
